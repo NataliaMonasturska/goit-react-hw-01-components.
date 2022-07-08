adfcb11a5412efb6c9700d00aebbaf2473fcbaa8
Natalia Monasturska <tjomamon@gmail.com>
2022-07-08 14:06:34 +0300

    Profile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,11 @@
 import {Profile} from 'components/Profile/Profile';
-import user from '../user.json';
+import user from '../../user.json';
+import css from './App.module.css';
 
 
 export const App = () => {
   return (
-    <div >
+    <div className={css.container}>
     <Profile 
       username={user.username}
       tag={user.tag}
