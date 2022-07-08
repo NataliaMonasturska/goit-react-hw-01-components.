db30af1a309e58df3e58bd37ef93bd242c1adc56
Natalia Monasturska <tjomamon@gmail.com>
2022-07-09 00:48:19 +0300

    friends

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,9 @@
 import {Profile} from 'components/Profile/Profile';
-import {Statistics} from 'components/Statistics/Statistics'
+import {Statistics} from 'components/Statistics/Statistics';
+import {FriendList } from 'components/FriendList/FriendList';
 import user from '../../user.json';
 import data from '../../data.json';
+import friends from '../../friends.json';
 import css from './App.module.css';
 
 
@@ -16,6 +18,7 @@ export const App = () => {
       stats={user.stats}
     />
     <Statistics title="Upload stats" stats={data} />
+    <FriendList friends={friends} />
     </div>
   );
 };
